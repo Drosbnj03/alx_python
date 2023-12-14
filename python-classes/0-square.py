6256c1ba1f6d429fbb1b64ba63459502bddfb3c6
Achraf <achrafdrs03@outlook.com>
2023-12-14 19:55:51 +0100

    t

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+"""This module houses a class that represents a square """
+class Square:
+    """Represents a Square"""
+    __size = None
+    def __init__(self,__size):
+        self.__size = __size
