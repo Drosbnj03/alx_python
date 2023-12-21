b5e3781167090827376da3d0b816a6fc5ce0adca
Achraf <achrafdrs03@outlook.com>
2023-12-21 12:44:47 +0100

    5

--- python-network/0-hbtn_status.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url ='https://alu-intranet.hbtn.io/status'
-res = requests.get(url)
-
-print("Body response:")
-print("\t- type: {}".format(type(res.text)))
-print("\t- content: {}".format(res.text))
